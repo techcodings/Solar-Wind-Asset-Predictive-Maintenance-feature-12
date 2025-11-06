2b576aa35c7a3e72d0de253be0ff69d485835a98
Goutham <lmgoutham8@gmail.com>
2025-11-06 15:46:12 +0530

    chanesg3

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()], server:{ port: 5177 } })
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',     // ✅ ensures correct relative paths after build
+})
